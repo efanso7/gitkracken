29a19992fededbbe1103e34e04b9d4866c8307b3
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-11-20 10:19:36 -0500

    add count blocks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
 public class Main {
+    //this function wil determine how many blocks needed for a pyramid
+    public static void countBlocks(int levels) {
+        int total = 0;
+        for (int i = 1; i <= levels; i++) {
+            total = total + (i * i);
+        }
+        System.out.println(total);
+    }
+
 
     public static void main(String[] args) {
-	// create inner and outer four loop
+        // create inner and outer four loop
         for(int i=0; i<3; i++){
             System.out.println("Outer Loop: " + i);
             for(int j=0; j<5; j++){
                 System.out.println("\tInner Loop " + j + ": Hi Everyone!");
             }
         }
+        countBlocks(5);
     }
 }
