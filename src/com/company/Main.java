778f0486b1d12453174bafa4676e1013608c60ef
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-11-20 10:14:48 -0500

    add inner and outer four loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// create inner and outer four loop
+        for(int i=0; i<3; i++){
+            System.out.println("Outer Loop: " + i);
+            for(int j=0; j<5; j++){
+                System.out.println("\tInner Loop " + j + ": Hi Everyone!");
+            }
+        }
     }
 }
